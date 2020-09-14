f2ba777fe60b6b848e1d719d4b88dcdcc32aa531
HansFarro <farrocastillohans@gmail.com>
2020-09-14 14:54:24 -0500

    navbar-responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,28 +10,28 @@ const Navbar = () => {
     };
 
     return (
-                    <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
-                        <a onClick={() => scroll.scrollToTop(scrollType)} className="navbar-brand" href="#info">Hans Farro Castillo</a>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarColor01">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#about">SOBRE MÍ</a>
-                                </li> 
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#skill">HABILIDADES</a>
-                                </li> 
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#projects">PROYECTOS</a>
-                                </li> 
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#contact">CONTÁCTAME</a>
-                                </li> 
-                            </ul>
-                        </div>
-                    </nav>
+        <nav className="navbar navbar-expand-md navbar-expand-lg navbar-dark bg-primary sticky-top">
+            <a onClick={() => scroll.scrollToTop(scrollType)} className="navbar-brand" href="#info">Hans Farro Castillo</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarColor01">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#about">SOBRE MÍ</a>
+                    </li> 
+                    <li className="nav-item">
+                        <a className="nav-link" href="#skill">HABILIDADES</a>
+                    </li> 
+                    <li className="nav-item">
+                        <a className="nav-link" href="#projects">PROYECTOS</a>
+                    </li> 
+                    <li className="nav-item">
+                        <a className="nav-link" href="#contact">CONTÁCTAME</a>
+                    </li> 
+                </ul>
+            </div>
+        </nav>
      );
 }
  
