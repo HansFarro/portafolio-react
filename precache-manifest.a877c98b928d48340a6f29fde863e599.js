33ff3a6cba135085ce8a16b66d16d93b5b088c57
HansFarro <farrocastillohans@gmail.com>
2020-09-13 23:49:55 -0500

    Updates

---
+++ precache-manifest.a877c98b928d48340a6f29fde863e599.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f5f285066cf9fba445305bdda4ff39",
+    "url": "/portafolio-react/index.html"
+  },
+  {
+    "revision": "ecb0e8b422f076328fec",
+    "url": "/portafolio-react/static/css/2.6d04c983.chunk.css"
+  },
+  {
+    "revision": "e05a6f1f4081a55554cb",
+    "url": "/portafolio-react/static/css/main.17043d5f.chunk.css"
+  },
+  {
+    "revision": "ecb0e8b422f076328fec",
+    "url": "/portafolio-react/static/js/2.78ccafc2.chunk.js"
+  },
+  {
+    "revision": "2cdcd9dc8104691b4491c04bf6c36bcb",
+    "url": "/portafolio-react/static/js/2.78ccafc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e05a6f1f4081a55554cb",
+    "url": "/portafolio-react/static/js/main.247d04d5.chunk.js"
+  },
+  {
+    "revision": "0db321651ef8cba96924",
+    "url": "/portafolio-react/static/js/runtime-main.07553a14.js"
+  },
+  {
+    "revision": "c583d5d562e221d704e6869e67c6e16c",
+    "url": "/portafolio-react/static/media/coding_two_color.c583d5d5.svg"
+  },
+  {
+    "revision": "1ff7e916596c1a79ca64c2b5fc788958",
+    "url": "/portafolio-react/static/media/javascript.1ff7e916.svg"
+  },
+  {
+    "revision": "7fd627bc33b8f32cc1a622596af1e2fc",
+    "url": "/portafolio-react/static/media/profile-image.7fd627bc.svg"
+  }
+]);
